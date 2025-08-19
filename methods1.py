d585223671440dfb16405643acc6acb2a8ccea2a
Vaishnavi Shetty K <vaishnavishettyk@Vaishnavis-MacBook-Air.local>
2025-08-19 21:02:34 +0530

    initial commit

---
+++ methods1.py
@@ -0,0 +1,14 @@
+class Demo:
+    def __init__(self):
+        self.a=10
+        self.b=20
+
+    def disp(self):
+        x=100
+        y=200
+        z=x+y
+        print(z)
+d1=Demo()
+print(d1.a)
+print(d1.b)
+d1.disp()
